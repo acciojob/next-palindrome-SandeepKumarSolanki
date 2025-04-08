9bc15035cc743a24182f6eece5ba78adad09ddcf
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-09 01:00:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function nextPalindrome(num) {
+	num = +num
   //your JS code here.
+	for(let i=num+1 ; i<Infinity ; i++){
+		let original = i ; 
+		let sum = 0;
+		while(original != 0){
+			let rem = original % 10;
+			sum = (sum * 10 + rem);
+			original = Math.floor(original/10);
+		}
+		if(sum === i){
+			return i;
+		}
+	}
 }
 
 const input = prompt("Enter a palindrome number");
